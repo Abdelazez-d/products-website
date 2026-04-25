2881f9341b07c7652bb6c8b19f856e4ea3b2cbc9
Abdelazez-d <zizo2182008888@gmail.com>
2026-04-25 21:14:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+age = prompt('What is your age ?');
+age > 18? 
+document writeln('hello user')
+:age == 18?
+document .writelnI('you are 18 now hello')
+document .writelnI('no you are very young');
+
